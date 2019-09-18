2572db6126e1100eca51477896170a756f45c3bb
SteamOS Desktop <desktop@steamos>
2019-09-17 20:41:15 -0600

    Alpha-2 is for new server

--- gulp-tasks/server.js
+++ gulp-tasks/server.js
@@ -5,11 +5,11 @@ var path    = require('path');
 
 module.exports = function(done) {
     var app = express();
-    var port = process.env.PORT || 80;
+    var port = process.env.PORT || 8080;
     app.use(express.static(path.join(__dirname, '..')));
 
     app.listen(port, function() {
-        console.log('Listening on http://localhost:' + port + ' for requests.');
+        console.log('Listening on http://192.168.0.37:' + port + ' for requests.');
         done();
     });
 };
